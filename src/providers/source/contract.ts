b281a06e80e54957cd0db5d6515d08d2bd160483
Serg <serg.zgama@zxteam.net>
2019-04-23 19:25:55 +0300

    New contract for Source provider

--- src/providers/source/contract.ts
+++ src/providers/source/contract.ts
@@ -1,5 +1,5 @@
-import { CancellationToken, Task as TaskLike } from "@zxteam/contract";
 import { price } from "../../PriceService";
+import { CancellationToken, Task as TaskLike } from "@zxteam/contract";
 
 export interface SourceProvider {
 	sourcesytemId: string;
@@ -8,17 +8,13 @@ export interface SourceProvider {
 	 * Loading empty price from sources
 	 * @param cancellationToken Cancellation Token allows your to cancel execution process
 	 * @param loadArgs Arguments for which need to sync prices
-	 * @error LimitError If breaking a request rate limit to source
-	 * @error BrokenProviderError If source provide returm wrong data
-	 * @error NoRecordError If don't exist data for arguments
-	 * @error ConnectionError If it failed to connect to source
+	 * @error NoDataError If don't exist data for arguments
+	 * @error CommunicationError If it failed to connect to source
 	 * @error BrokenApiError Something happened wrong on service
 	 */
 	loadPrices(cancellationToken: CancellationToken, loadArgs: price.LoadDataRequest): TaskLike<price.HistoricalPrices>;
-
 }
 
-export class NoRecordError extends Error { }
-export class BrokenProviderError extends Error { }
-export class ConnectionError extends Error { }
+export class NoDataError extends Error { }
+export class CommunicationError extends Error { }
 export class BrokenApiError extends Error { }
