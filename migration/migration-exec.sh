de142628c6fe5d5579e1603f746a1c9b2c857c41
Maxim Anurin <m.anurin@wnb.com.ua>
2020-10-23 14:05:47 +0300

    CP-000 Playing with shell date calculation. Change devel-1EVO:install:db image to bitnami/kubectl

--- migration/migration-exec.sh
+++ migration/migration-exec.sh
@@ -109,9 +109,11 @@ echo "# Cleanuping oldest jobs..."
 if [ "$(uname)" = "Darwin" ]; then
 	#  Mac OS X platform
 	OBSOLEBE_TIMESTAMP=$(date -u -v-6m '+%Y%m%d%H%M%S')
-else
+elif [ "$(readlink /bin/date)" = "/bin/busybox" ]; then
 	# 60 seconds * 60 minutes * 24 hours * 180 days
-	OBSOLEBE_TIMESTAMP=$(busybox date -d "$(( `busybox date +%s`-60*60*24*180 ))" '+%Y%m%d%H%M%S')
+	OBSOLEBE_TIMESTAMP=$(date -d "$(( `date +%s`-60*60*24*180 ))" '+%Y%m%d%H%M%S')
+else
+	OBSOLEBE_TIMESTAMP=$(date -d "6 month ago" '+%Y%m%d%H%M%S')
 fi
 
 for EXIST_JOB in $(kubectl ${KUBE_OPTS} get jobs -o go-template --template='{{range .items}}{{.metadata.name}} {{end}}'); do
