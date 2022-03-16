622ff806c4656d831dc66ecc16a87ea6d6f6a2fc
Anatoliy Vasilev <a.vasilev@wnb.com.ua>
2022-03-16 16:52:06 +0200

    Update support-bot to 2.0.4

--- runtime.scripts/download-configmaps-and-secrects.sh
+++ runtime.scripts/download-configmaps-and-secrects.sh
@@ -73,3 +73,11 @@ case "${KUBE_NAMESPACE}" in
 		download "Secrets/supportbot"                          "${TARGET_DIRECTORY}/secret-supportbot.yaml"
 		;;
 esac
+
+case "${KUBE_NAMESPACE}" in
+	cexpay-evolution-admin|cexpay-presentation-admin|cexpay-test-admin)
+		# Support bot should be presented only in evolution, presentation and test zones
+		download "ConfigMap/gateintegrationdemo-files"         "${TARGET_DIRECTORY}/configmap-gateintegrationdemo-files.yaml"
+		;;
+esac
+
